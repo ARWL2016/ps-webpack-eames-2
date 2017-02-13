895821e3df84b482f046b5f953f3e8188ccd260e
Alistair Willis <alistairrwillis@gmail.com>
2017-02-13 11:43:29 +0900

    add css-loaders

--- README.md
+++ README.md
@@ -12,8 +12,13 @@ https://github.com/ARWL2016/ps-webpack-eames-2
 **Packages**   
 1. webpack (-g)  
 2. webpack-dev-server (-g) - with hot reloading   
-3. babel, babel loader and babel-preset-2015  
-4. jshint and jshint-loader  
-5. strip-loader  
-6. http-server (-g)   
+
+---
+####Adding CSS to the Build  
+*Conventionally, we include css with a link ref inside `index.html`. But with webpack, we can include css in the **js module system** using a require statement and loaders. App.js becomes the root of our project instead of index.html.* 
+1. `npm install style-loader css-loader --save-dev`  
+2. remove style links from `index.html`  
+3. In `webpack.config` under `module.rules` create a new loader object as before, with test, exclude and loader properties
+4. Set `loader: "style-loader!css-loader"`. The `!` syntax is common in webpack. It will run our css files first through css-loader and then through style-loader. 
+5. In `app.js`, use a require statement and the path to import the css files. E.g. `require("../css/app.css")` If there is a bootstrap and a local style, we should include bootstrap first.  
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,25 @@
 var path = require('path'); 
-var webpack = require('webpack');
-
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('shared'); 
 
 module.exports = {
     context: path.resolve('js'), 
-    // entry: ["./utils", "./app"], - single bundle
-    entry: {
-        about: './about_page.js', 
-        home: './home_page.js',
-        contact: './contact_page.js'  
-    },
+    entry: ["./app"],
     output: {
         path: path.resolve('build/js'),
         publicPath: '/public/assets/js/', 
-        // filename: "bundle.js" - single bundle
-        filename: "[name].js" //matches entry
+        filename: "bundle.js" 
     }, 
 
-    plugins: [commonsPlugin], 
     devServer: {
         contentBase: 'public'
     }, 
-    watch: false, 
 
     module: {
         rules: [
             {
-                enforce: 'pre',
-                test: /\.js$/, 
+                test: /\.css$/, 
                 exclude: /node_modules/, 
-                loader: "jshint-loader"
+                loader: "style-loader!css-loader" 
             },
-            {
-                test: /\.es6$/, 
-                exclude: /node_modules/, 
-                loader: "babel-loader"
-            }
         ] 
     }, 
 
